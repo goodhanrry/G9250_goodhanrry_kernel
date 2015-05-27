3a7cf5b1b8a9297aa0e4b6ad2139cff34a5496d3
goodhanrry <hanrry@21cn.com>
2015-05-27 15:07:59 +0800

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#3 SMP PREEMPT Wed Apr 29 17:03:57 CST 2015"
+#define LINUX_COMPILE_BY "hanrry"
+#define LINUX_COMPILE_HOST "hanrry-X201E"
+#define LINUX_COMPILER "gcc version 4.9.x-google 20140827 (prerelease) (GCC) "
